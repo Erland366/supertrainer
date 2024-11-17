4704e8078230484abe21462033eb69987104d513
Rifo Genadi <rifogenadi@MacBookPro.mbzu.ae>
2024-11-17 23:16:26 +0400

    add readme to use ms-swift training script

--- scripts/ai701/train_swift.sh
+++ scripts/ai701/train_swift.sh
@@ -15,9 +15,8 @@ export CUDA_VISIBLE_DEVICES=0
 # Run the swift sft command with the specified model type
 swift sft \
     --model_type "$MODEL_TYPE" \
-    --dataset /home/rifo.genadi/material_classification/train_internvl.jsonl \
+    --dataset train.jsonl \
     --max_length 144 \
-    --hub_token b07e1120-5a1f-4044-84cb-1cb3c2e415fb \
     --per_device_train_batch_size 4 \
     --gradient_accumulation_steps 8 \
     --num_train_epochs 2 \
